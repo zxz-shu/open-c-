e042932a833e2c0b4cb0be29834c7cf83c317301
zxz-shu <zhixuanzhe@gmail.com>
2024-06-29 14:55:54 +0800

    hello world
    there  is not thing

---
+++ 父母性别.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+string a;
+cout << "输入：" ;
+for(int i=0;i<1;i++){
+cin >> a;
+if(a=="父亲")cout << "男" << endl;
+else cout << "女" << endl;
+}
+return 0;}//C++移植版 Github 已开源
